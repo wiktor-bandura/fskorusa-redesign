5c8bf56ff5836fc122d7e582cc0441066dd043b2
System Administrator <band.wiktor@gmail.com>
2024-07-30 20:02:13 +0200

    More styles for front page

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -5,7 +5,20 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const swiper = new Swiper(".timeline", {
-  spaceBetween: 30,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",

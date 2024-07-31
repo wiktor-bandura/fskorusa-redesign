e1232b1ec00a0262329b7365eb4f41076b36f45e
System Administrator <band.wiktor@gmail.com>
2024-07-31 19:10:08 +0200

    Added scss for memories, square and gallery section on front page

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -4,26 +4,30 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const swiper = new Swiper(".timeline", {
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+let swiper = null;
+
+if (document.querySelector(".timeline")) {
+  swiper = new Swiper(".timeline", {
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
     },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 50,
-    },
-  },
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+  });
+}
 
 export default swiper;
